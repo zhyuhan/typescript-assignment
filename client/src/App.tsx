ae737dce16d6a293ff291483f272fba044db3ffa
Yu Han Zhang <yuhan@zhyuhan.me>
2024-03-10 22:12:16 +0800

    feat: set up router and routes

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-const App = () => {
-    return (
-        <h1>Write your code here!</h1>
-    );
-};
-
-export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,25 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/home";
+import Root from "./routes/root";
+import Settings from "./routes/settings";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "settings",
+                element: <Settings />,
+            },
+        ],
+    },
+]);
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
