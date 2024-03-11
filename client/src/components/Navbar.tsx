6bd863900b5b1b87ffcb106cc86a6d24260ebcab
Yu Han Zhang <yuhan@zhyuhan.me>
2024-03-11 17:23:36 +0800

    feat: add navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1 className="nav-brand">Notification task</h1>
+
+            <ul>
+                <li className="nav-link">
+                    <NavLink to="/">Main</NavLink>
+                </li>
+                <li className="nav-link">
+                    <NavLink to="/settings">Settings</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import "../styles/main.css";
 
 export default function Root() {
     return (
