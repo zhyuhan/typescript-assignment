97b8c28d8a5a5af2e3ea3e3001c54a30aee9f76b
Yu Han Zhang <yuhan@zhyuhan.me>
2024-03-13 19:10:50 +0800

    feat: use more sensible options

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const BROADCAST_CHANNEL = "notifications";
 export const SERVER_ENDPOINT = "http://localhost:9000/events";
 
 export const DEFAULT_NOTIFICATION_OPTIONS: NotificationOptions = {
-    count: 3,
+    count: 5,
     position: "top-right",
-    duration: 2,
+    duration: 3,
 };
